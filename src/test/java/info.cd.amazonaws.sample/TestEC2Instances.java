1d20bc9e08f812c65cb7b10d06d30fba5c0f576a
tes23 <danger23@gmail.com>
2014-05-18 02:59:06 +0200

    Checking runnable instances in test

--- src/test/java/info.cd.amazonaws.sample/TestEC2Instances.java
+++ src/test/java/info.cd.amazonaws.sample/TestEC2Instances.java
@@ -5,7 +5,10 @@ import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.*;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -18,28 +21,33 @@ import static org.junit.Assert.*;
  */
 public class TestEC2Instances {
 
+    private Properties properties;
     private String instanceId;
+    private String imageId;
     private String endpoint;
+    private AmazonEC2Client ec2Client;
 
     @Before
     public void setUp() throws Exception {
-        Properties properties = new Properties();
+        properties = new Properties();
         properties.load(TestEC2Instances.class.getResourceAsStream("AwsInstances.properties"));
 
-        instanceId = properties.getProperty("instanceID");
-        endpoint = properties.getProperty("endpoint");
+        instanceId = getProperty("instanceID");
+        endpoint = getProperty("endpoint");
+        imageId = getProperty("imageID");
+
+        AWSCredentials credentials = new PropertiesCredentials(TestEC2Instances.class.getResourceAsStream("AwsCredentials.properties"));
+
+        ec2Client = new AmazonEC2Client(credentials);
+        ec2Client.setEndpoint(endpoint);
 
     }
 
-    @org.junit.Test
+    @Ignore
+    @Test
     public void testStartEC2Instance() throws Exception {
-        AWSCredentials credentials = new PropertiesCredentials(TestEC2Instances.class.getResourceAsStream("AwsCredentials.properties"));
-
-        AmazonEC2Client client = new AmazonEC2Client(credentials);
-        client.setEndpoint(endpoint);
-
         StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(instanceId);
-        StartInstancesResult result = client.startInstances(request);
+        StartInstancesResult result = ec2Client.startInstances(request);
 
 //        RunInstancesRequest request = new RunInstancesRequest("ami-2918e35e", 1, 1);
 //        RunInstancesResult result = client.runInstances(request);
@@ -47,11 +55,30 @@ public class TestEC2Instances {
         List<InstanceStateChange> instances = result.getStartingInstances();
         assertEquals(1, instances.size());
 
-//        DescribeInstancesRequest descRequest = new DescribeInstancesRequest();
-//        descRequest.setInstanceIds(Arrays.asList(instanceId));
-//
-//        DescribeInstancesResult descResult = client.describeInstances(descRequest);
-//        assertNotNull(descResult);
 
+
+    }
+
+    @Test
+    public void testRunnableInstances() throws Exception {
+        DescribeInstancesRequest descRequest = new DescribeInstancesRequest();
+        descRequest.setInstanceIds(Arrays.asList(instanceId));
+
+        DescribeInstancesResult descResult = ec2Client.describeInstances(descRequest);
+        assertNotNull(descResult);
+        List<Reservation> reservations = descResult.getReservations();
+        assertEquals(1, reservations.size());  //the list can not be null
+        List<Instance> instances = reservations.get(0).getInstances();
+        assertEquals(1, instances.size());    //the list can not be null
+        Instance instance = instances.get(0);
+        assertNotNull(instance);
+        assertEquals(instanceId, instance.getInstanceId());
+        assertEquals(imageId, instance.getImageId());
+        assertEquals("running", instance.getState().getName());
+
+    }
+
+    private String getProperty(String key) {
+        return properties != null ? properties.getProperty(key) : null;
     }
 }

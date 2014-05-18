7f2cd55e0903772f42176c00f6f77f9e230a45a2
tes23 <danger23@gmail.com>
2014-05-18 02:07:08 +0200

    Starting instance test case

--- src/test/java/info.cd.amazonaws.sample/TestEC2Instances.java
+++ src/test/java/info.cd.amazonaws.sample/TestEC2Instances.java
@@ -3,30 +3,55 @@ package info.cd.amazonaws.sample;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
-import com.amazonaws.services.ec2.model.RunInstancesResult;
-import org.junit.Assert;
+import com.amazonaws.services.ec2.model.*;
+import org.junit.Before;
+
+import java.util.List;
+import java.util.Properties;
 
 import static org.junit.Assert.*;
 
 /**
- * Created with IntelliJ IDEA.
- * User: tes
+ * Created by User: tes
  * Date: 5/17/14
  * Time: 5:41 PM
- * To change this template use File | Settings | File Templates.
  */
 public class TestEC2Instances {
+
+    private String instanceId;
+    private String endpoint;
+
+    @Before
+    public void setUp() throws Exception {
+        Properties properties = new Properties();
+        properties.load(TestEC2Instances.class.getResourceAsStream("AwsInstances.properties"));
+
+        instanceId = properties.getProperty("instanceID");
+        endpoint = properties.getProperty("endpoint");
+
+    }
 
     @org.junit.Test
     public void testStartEC2Instance() throws Exception {
         AWSCredentials credentials = new PropertiesCredentials(TestEC2Instances.class.getResourceAsStream("AwsCredentials.properties"));
 
         AmazonEC2Client client = new AmazonEC2Client(credentials);
-        client.setEndpoint("ec2.eu-west-1.amazonaws.com");
+        client.setEndpoint(endpoint);
 
-//        RunInstancesRequest request = new RunInstancesRequest("ami-2918e35", 1, 1);
-//        RunInstancesResult result = AmazonEC2Client.
+        StartInstancesRequest request = new StartInstancesRequest().withInstanceIds(instanceId);
+        StartInstancesResult result = client.startInstances(request);
+
+//        RunInstancesRequest request = new RunInstancesRequest("ami-2918e35e", 1, 1);
+//        RunInstancesResult result = client.runInstances(request);
+        assertNotNull(result);
+        List<InstanceStateChange> instances = result.getStartingInstances();
+        assertEquals(1, instances.size());
+
+//        DescribeInstancesRequest descRequest = new DescribeInstancesRequest();
+//        descRequest.setInstanceIds(Arrays.asList(instanceId));
+//
+//        DescribeInstancesResult descResult = client.describeInstances(descRequest);
+//        assertNotNull(descResult);
 
     }
 }
